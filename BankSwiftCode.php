af63ccc6e7a4f1e5d8ae8c75e687ff43dec350ed
Pierrick Gicquelais <pierrick.gicquelais@epitech.eu>
2017-05-02 11:02:56 +0200

    #1: Insert A* bank swift codes

---
+++ BankSwiftCode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the SecurityBundle package.
+ * Pierrick Gicquelais <pierrick.gicquelais@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * File that was distributed with this source code.
+ *
+ */
+
+/**
+ * Class BankSwiftCode
+ */
+class BankSwiftCode
+{
+
+}
